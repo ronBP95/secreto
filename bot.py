f8973b67b840692887e0b26af43652c87103675d
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-28 00:41:02 -0700

    Initialization successful

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 # bot.py
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
+print(TOKEN)
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
