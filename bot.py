577a088a96dc84988d66c72d2ca61da40b732352
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-29 21:19:00 -0700

    Added messaging

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} BOT has connected to Discord!')
 
 @client.event
 async def on_message(message):
