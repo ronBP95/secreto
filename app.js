103a42785ff61b9cbab8fc83e4d900bad842d181
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-11-09 21:08:24 -0800

    testing slash commands

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 require('dotenv').config(); //initialize dotenv
-const { Client, GatewayIntentBits } = require('discord.js'); //import discord.js
+const { Client, Events, GatewayIntentBits } = require('discord.js'); //import discord.js
 
 const client = new Client({
   intents: [
 		GatewayIntentBits.MessageContent,
+    GatewayIntentBits.Guilds,
   ]
 }); //create new client
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
 
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
