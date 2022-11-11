fc2ffdfb34176c74efdb38774f1276571bc8c88e
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-11-10 17:20:23 -0800

    Removed previous code because I'm looking for event hhandling before slash commands

--- app.js
+++ app.js
@@ -12,14 +12,5 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-});
-
-
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
